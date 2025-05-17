38053e412ff52317559272944959235bd75aa4d4
binjha <purohitvijay03112003@gmail.com>
2025-05-17 03:25:48 +0000

    java problem basics using println

---
+++ challange 1/problem3.java
@@ -0,0 +1,11 @@
+public class problem3{
+    public static void main(String[] args) {
+        System.out.println("    *");
+        System.out.println("   **");
+        System.out.println("  ***");
+        System.out.println(" ****");
+        System.out.println("*****");
+//learning:- the whitspaces givien between the strings are always accepted as the above code example 
+
+    }
+}
---
+++ challange 1/promblem1.java
@@ -0,0 +1,5 @@
+public class promblem1{
+    public static void main(String[] args) {
+        System.out.println("Good Morning Binjha");
+    }
+}
