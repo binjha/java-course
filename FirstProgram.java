9cb9eb34aef09070fc40a31b0dc5090167b460d0
binjha <purohitvijay03112003@gmail.com>
2025-05-17 03:16:01 +0000

    java first program

---
+++ FirstProgram.java
@@ -0,0 +1,11 @@
+
+
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.print("Hello Binha ");
+        System.out.println("Hello ");
+        //print only print in one line whereas println work as /n giving next line 
+    }
+    
+}
