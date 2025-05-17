de1eda28c4c4c2c10859e672af920d325eccb2e0
binjha <purohitvijay03112003@gmail.com>
2025-05-17 06:22:38 +0000

    variables

---
+++ variables/variable.java
@@ -0,0 +1,30 @@
+public class variable{
+    public static void main(String[] args) {
+        int myNumber =4;
+        System.out.println("myNumber");
+
+        int yourNumber=44;
+        System.out.println(yourNumber);
+        yourNumber=33;
+        System.out.println(yourNumber);
+
+        float myFloat=44.0f;
+        System.out.println(myFloat);
+
+        boolean myBoolean=true;
+        System.out.println(myBoolean);
+
+        myBoolean=false;
+        System.out.println(myBoolean);
+
+        String myString="Binjha Boy";
+        System.out.println(myString);
+
+        
+
+
+
+
+
+    }
+}
