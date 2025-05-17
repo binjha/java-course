4e5c861131f0523e7f6d6e868836efe1372c608d
binjha <purohitvijay03112003@gmail.com>
2025-05-17 06:49:25 +0000

    escape

---
+++ variables/escape.java
@@ -0,0 +1,23 @@
+//escape sepuences 
+
+public  class escape{
+    public static void main(String[] args) {
+        System.out.println("hello binjha ");
+        System.out.println("hello 'binjha'");
+
+        //so what about the double quotes 
+        System.out.println("hello \"binjha\"... ");
+    
+
+        System.out.println("\\binjha\\");
+        System.out.println("\"binjha\"");
+
+        System.out.println("hello \nBinjha");
+        System.out.println("hello \tBinjha");
+        System.out.println("hello\bBinjha");//insert a backspace at the text at this point
+
+
+
+
+    }
+}
