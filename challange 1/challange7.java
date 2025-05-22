5d299fa95cec3950ce6089d926f71f3718676c91
binjha <purohitvijay03112003@gmail.com>
2025-05-22 09:43:15 +0000

    commit

---
+++ challange 1/challange7.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class challange7 {
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        System.out.print("Enter the number 1: ");
+        int num1=input.nextInt();
+        
+        System.out.print("Enter the number 2: ");
+        int num2=input.nextInt();
+
+        System.out.println("sum= "+(num1+num2));
+        System.out.println("subtraction= "+(num1-num2));
+        System.out.println("multiplication= "+(num1*num2));
+        System.out.println("division= "+(num1/num2));
+        System.out.println("modulus= "+(num1%num2));
+
+
+
+
+
+
+
+
+
+
+
+
+    } 
+}
