bdb3c7e045433686e5f8636945f0f794d866c791
binjha <purohitvijay03112003@gmail.com>
2025-05-22 09:51:07 +0000

    commit

---
+++ challange 1/problem8.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class problem8 {
+    public static void main(String[] args) {
+
+        Scanner input=new Scanner(System.in);
+        System.out.println("Entet the first num: ");
+         float num1=input.nextFloat();
+        System.out.println("Entet the second num: ");
+         float num2=input.nextFloat();
+
+          float product=num1*num2;
+          System.out.println("The product is " +product);
+
+
+
+
+        // float num1=5.25f;
+        // float num2=4.25f;
+
+
+
+    }
+}
