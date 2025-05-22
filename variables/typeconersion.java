31cce9949329018af90996d47e402a6644763788
binjha <purohitvijay03112003@gmail.com>
2025-05-22 03:52:53 +0000

    commit

---
+++ variables/typeconersion.java
@@ -0,0 +1,17 @@
+public class typeconersion {
+    public static void main(String[] args) {
+        float myfloat=5.235f;
+
+        System.out.println(myfloat);
+        myfloat=(int)myfloat;
+        System.out.println(myfloat);
+
+        int myint=(int)5.4f;
+        System.out.println(myint);
+
+
+
+
+        
+    }
+}
