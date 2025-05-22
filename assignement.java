682dadf762468bc66c69b22ccb8216574b6445b7
binjha <purohitvijay03112003@gmail.com>
2025-05-22 04:29:19 +0000

    changes

---
+++ assignement.java
@@ -0,0 +1,13 @@
+
+
+public class assignement {
+    public static void main(String[] args) {
+        int myint=4;
+        int newint=myint;
+        System.out.println(myint);
+        System.out.println(newint);
+
+
+    }
+    
+}
