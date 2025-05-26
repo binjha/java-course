f4e01d1a82d16c61cbda5529412adc42272adec2
binjha <purohitvijay03112003@gmail.com>
2025-05-26 15:28:42 +0000

    push

--- challange 1/evenandodd.java
+++ challange 1/evenandodd.java
@@ -2,9 +2,16 @@ import java.util.Scanner;
 
 public class evenandodd {
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
+        Scanner input =new Scanner(System.in);
         System.out.println("Entert the number");
         int num1=input.nextInt();
+
+        if ((num1 & 1)==1) {
+            System.out.println("The number is odd ");
+            
+        }else{
+            System.out.println("The number is even ");
+        }
 
         
 
