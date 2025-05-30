6f61352b8e21cc34ec7c6e31b4b38a0dc6cf7df1
binjha <purohitvijay03112003@gmail.com>
2025-05-30 08:13:49 +0530

    commit

--- challange 1/fibonachhi.java
+++ challange 1/fibonachhi.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class Fibonacci {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Welcome to Fibonacci Series");
+        System.out.println("Welcome to Fibonacci Series:");
         System.out.print("Please enter the number up to which series has to be printed: ");
         int num = input.nextInt();
         System.out.println("Here is the Fibonacci Series");
